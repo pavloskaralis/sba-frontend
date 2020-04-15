044679c331f2a1047025dfcf55c3c1724d83ddde
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-04-14 23:13:05 -0400

    test

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static(__dirname ));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(process.cwd() + '/index.html'));
-});
+res.sendFile(path.join(__dirname + '/src/index.html'));
+}); 
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
